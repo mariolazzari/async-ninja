eada413a593e98507b59f0779a31dafb8de18360
Mario Lazzari <mario.lazzari@gmail.com>
2020-09-15 20:25:43 +0200

    Done lesson 3

--- sandbox.js
+++ sandbox.js
@@ -2,8 +2,10 @@ const request = new XMLHttpRequest();
 
 request.addEventListener("readystatechange", () => {
   // console.log(request, request.readyState);
-  if (request.readyState === 4) {
+  if (request.readyState === 4 && request.status === 2000) {
     console.log(request.responseText);
+  } else if (request.readyState === 4) {
+    console.log("Could not fetch data.");
   }
 });
 
