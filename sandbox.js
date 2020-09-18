07cd5aa74dd85d4a850b2f7de9c3e4723be9a3f5
Mario Lazzari <mario.lazzari@gmail.com>
2020-09-18 23:03:56 +0200

    Done lesson 6

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,23 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
   const request = new XMLHttpRequest();
   request.addEventListener("readystatechange", () => {
     // console.log(request, request.readyState);
     if (request.readyState === 4 && request.status === 2000) {
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
       //console.log(request.responseText);
     } else if (request.readyState === 4) {
       callback("Could not request data.");
       //console.log("Could not fetch data.", undefined);
     }
   });
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+  //request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+  request.open("GET", resource);
   request.send();
 };
 
-getTodos((err, data) => {
+// callback hell
+getTodos("todos.json", (err, data) => {
   if (err) {
     console.log(err);
   } else {
